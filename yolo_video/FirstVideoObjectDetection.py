378515a3a58276c0d02bd94a4e9c42562224861c
pparkitny <pat.parkitny@gmail.com>
2020-11-21 17:28:40 +0100

    Folder arrangement

--- yolo_video/FirstVideoObjectDetection.py
+++ yolo_video/FirstVideoObjectDetection.py
@@ -6,9 +6,9 @@ execution_path = os.getcwd()
 detector = VideoObjectDetection()
 detector.setModelTypeAsYOLOv3()
 detector.setModelPath(os.path.join(
-    execution_path, "weights/yolo.h5"))
+    execution_path, "weights/test.h5"))
 detector.loadModel()
 
 video_path = detector.detectObjectsFromVideo(input_file_path=os.path.join(execution_path, "media/video.mp4"), output_file_path=os.path.join(
-    execution_path, "traffic_detected"), frames_per_second=30, log_progress=True)
+    execution_path, "video2"), frames_per_second=30, log_progress=True)
 print(video_path)
