7c3028f012d1029e1c993042d30ec8630daf718e
pparkitny <pat.parkitny@gmail.com>
2020-12-02 22:28:05 +0100

    Whole interface changes
    - Changed whole interface

--- static/interface/js/jqscrollto.js
+++ static/interface/js/jqscrollto.js
@@ -3,7 +3,7 @@
             //zresetuj scrolla
             $.scrollTo(0);
             
-            $('#link1').click(function() { $.scrollTo($('#instruction').offset().top - 100, 500); });
+            $('#link1').click(function() { $.scrollTo($('#instruction').offset().top, 700); });
             $('#link2').click(function() { $.scrollTo($('#upload').offset().top - 170, 500); });
             $('#link3').click(function() { $.scrollTo($('#authors'), 500); });
             
@@ -15,9 +15,21 @@
         
         $(window).scroll(function()
         {
+            var content = $(".content-div")
             
             if($(this).scrollTop()>300) $('.scrollup').fadeIn();
             else $('.scrollup').fadeOut();
         
+
+            if($(this).scrollTop()>=window.innerHeight - 1){
+                $(".nav-main").addClass("nav-fixed")
+                content.css("position", "relative")
+
+            }
+            else{
+                $(".nav-main").removeClass("nav-fixed")
+                content.css("position", "unset")
+            }
+
         }
         );
